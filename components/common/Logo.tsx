ba3fbf867ba3a012495dea0f9e6071aeb770c6a8
br-mendes <brunoafonso.mendes@gmail.com>
2026-01-15 21:20:20 -0300

    Create Logo.tsx

---
+++ components/common/Logo.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './Logo.module.css';
+
+interface LogoProps {
+  size?: 'sm' | 'md' | 'lg';
+  animated?: boolean;
+}
+
+export const Logo: React.FC<LogoProps> = ({ size = 'md', animated = true }) => {
+  const sizeClass = styles[size];
+  const animatedClass = animated ? styles.animated : '';
+
+  return (
+    <div className={`${styles.logo} ${sizeClass} ${animatedClass}`}>
+      <div className={styles.icon}></div>
+      <span className={styles.text}>Divvy</span>
+    </div>
+  );
+};
